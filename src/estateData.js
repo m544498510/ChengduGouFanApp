009b81bcd46cf53e1bd24a29f79ad74bca2b4714
firefly_may <m544498510@gmail.com>
2018-03-24 23:18:18 +0800

    add the spider

---
+++ src/estateData.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const qs = require('qs');
+
+let estateData = [];
+function ajaxProjects(pageNum=1) {
+  if(pageNum === 1){
+    estateData = [];
+  }
+  const data = qs.stringify({
+    pageNo: pageNum,
+    regioncode: '00'
+  });
+  return axios.post('http://171.221.172.13:8888/lottery/accept/projectList', data)
+    .then(data => {
+      let ajaxNextPage = true;
+      const $ = cheerio.load(data.data);
+      const trs = $('#_projectInfo').find('tr');
+      for (let i = 0; i < trs.length; i++) {
+        let item = trs.eq(i);
+        const tds = $(item).find('td');
+        const projectName = tds.eq(3).text();
+        const status = tds.eq(10).text().trim();
+        if(status !== '报名结束'){
+          estateData.push({
+            id: tds.eq(0).text(),
+            name: projectName,
+            status,
+            count: tds.eq(6).text(),
+            startTime: tds.eq(8).text(),
+            endTime: tds.eq(9).text()
+          })
+        }else{
+          ajaxNextPage = false;
+          break;
+        }
+      }
+      if(ajaxNextPage){
+        ajaxProjects(pageNum+1);
+      }
+      console.log(estateData);
+    });
+}
+
+function ajaxDetail(id){
+  return axios.post('', qs.stringify({projectUuid:id}))
+    .then(data=>{
+      return data.data.message;
+    })
+}
+
+module.exports = {
+  ajaxDetail,
+  ajaxProjects
+};
